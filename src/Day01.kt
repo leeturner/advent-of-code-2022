2c12b7b35ef8628d2a1f7e3a05101a2a5e4e1c45
leeturner <lee@leeturner.me>
2022-12-01 09:29:47 +0000

    feat: AOC day 1 solution

--- src/Day01.kt
+++ src/Day01.kt
@@ -1,17 +1,23 @@
 fun main() {
-    fun part1(input: List<String>): Int {
-        return input.size
-    }
+  fun part1(input: List<List<Int>>): Int {
+    return input.maxOf { it.sum() }
+  }
 
-    fun part2(input: List<String>): Int {
-        return input.size
-    }
+  fun part2(input: List<List<Int>>): Int {
+    return input.map { it.sum() }.sortedDescending().take(3).sum()
+  }
 
-    // test if implementation meets criteria from the description, like:
-    val testInput = readInput("Day01_test")
-    check(part1(testInput) == 1)
+  // test if implementation meets criteria from the description, like:
+  val testInput =
+      readInputSplitBy("Day01_test", "\n\n").map {
+        it.split("\n").map { calories -> calories.toInt() }
+      }
+  check(part1(testInput) == 24000)
+  check(part2(testInput) == 45000)
 
-    val input = readInput("Day01")
-    println(part1(input))
-    println(part2(input))
+  val input = readInputSplitBy("Day01", "\n\n").map {
+    it.split("\n").map { calories -> calories.toInt() }
+  }
+  println(part1(input))
+  println(part2(input))
 }
