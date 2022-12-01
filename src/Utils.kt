40f754db69d50cbc66030117caadb3579936d277
leeturner <lee@leeturner.me>
2022-12-01 09:29:15 +0000

    feat: new util function to read a text file and split it into a list of strings by a given delimiter

--- src/Utils.kt
+++ src/Utils.kt
@@ -2,15 +2,15 @@ import java.io.File
 import java.math.BigInteger
 import java.security.MessageDigest
 
-/**
- * Reads lines from the given input txt file.
- */
-fun readInput(name: String) = File("src", "$name.txt")
-    .readLines()
+/** Reads lines from the given input txt file. */
+fun readInput(name: String) = File("src", "$name.txt").readLines()
 
-/**
- * Converts string to md5 hash.
- */
-fun String.md5() = BigInteger(1, MessageDigest.getInstance("MD5").digest(toByteArray()))
-    .toString(16)
-    .padStart(32, '0')
+/** Reads lines from the given input txt file plit by a given delimiter. */
+fun readInputSplitBy(name: String, delimiter: String) =
+    File("src", "$name.txt").readText().split(delimiter)
+
+/** Converts string to md5 hash. */
+fun String.md5() =
+    BigInteger(1, MessageDigest.getInstance("MD5").digest(toByteArray()))
+        .toString(16)
+        .padStart(32, '0')
